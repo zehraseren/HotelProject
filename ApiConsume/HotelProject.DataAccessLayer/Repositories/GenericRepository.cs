a1b9cd8d96004284ae2cf25b0b895fef79b9ed9c
Zehra Seren <fatmazehraseren@gmail.com>
2024-07-31 20:36:24 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T t)
+        {
+            _context.Remove(t);
+            _context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
