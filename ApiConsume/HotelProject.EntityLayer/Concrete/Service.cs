4401ae1d1757c5fed6a74fa194f93092b4655103
Zehra Seren <fatmazehraseren@gmail.com>
2024-08-02 21:51:12 +0300

    Entities updated.

--- ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Service.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelProject.EntityLayer.Concrete
+﻿namespace HotelProject.EntityLayer.Concrete
 {
     public class Service
     {
