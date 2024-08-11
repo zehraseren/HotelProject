0859580d0593d362d50cc9726bba77ee92e94939
Zehra Seren <fatmazehraseren@gmail.com>
2024-08-11 20:16:25 +0300

    Booking admin page is done.

---
+++ FrontEnd/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using HotelProject.WebUI.Dtos.BookingDto;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("http://localhost:5200/api/Booking/");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
