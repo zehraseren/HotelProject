72a1d6abb686e59bd66f2bafe8682b90de5bb46a
Zehra Seren <fatmazehraseren@gmail.com>
2024-08-16 02:03:04 +0300

    JWT web token is done.

---
+++ JWTProject/WebApiJWT/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using WebApiJWT.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApiJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Token()
+        {
+            return Ok(new CreateToken().TokenCreate());
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult TokenForRole()
+        {
+            return Ok(new CreateToken().TokenCreateForRole());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Auth()
+        {
+            return Ok("Welcome");
+        }
+
+        [Authorize(Roles = "Admin, Visitor")]
+        [HttpGet("[action]")]
+        public IActionResult AuthForRole()
+        {
+            return Ok("The entry was successful.");
+        }
+    }
+}
---
+++ JWTProject/WebApiJWT/Models/CreateToken.cs
@@ -0,0 +1,38 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApiJWT.Models
+{
+    public class CreateToken
+    {
+        public string TokenCreate()
+        {
+            var bytes = Encoding.UTF8.GetBytes("your-32-character-long-key-string");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddSeconds(20), signingCredentials: credentials);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+
+        public string TokenCreateForRole()
+        {
+            var bytes = Encoding.UTF8.GetBytes("your-32-character-long-key-string");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(bytes);
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "Visitor")
+            };
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddSeconds(30), signingCredentials: credentials, claims: claims);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
