6506ce7a627a78a34f2dc61ae74f24e1289c6cc7
Zehra Seren <fatmazehraseren@gmail.com>
2024-08-24 01:57:25 +0300

    Api file operations have been completed.

---
+++ ApiConsume/HotelProject.WebAPI/Controllers/FileProcessController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileProcessController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "files/" + fileName);
+            var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+            return Created("", file);
+        }
+    }
+}
---
+++ FrontEnd/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent
+                byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+
+            var httpClient = new HttpClient();
+            await httpClient.PostAsync("http://localhost:5200/api/FileImage/", multipartFormDataContent);
+
+            return View();
+        }
+    }
+}
